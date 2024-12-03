1d2bfe0131c78f000ffce55e3dc03a079f309a39
iamknownasfesal <mkircal957@gmail.com>
2024-12-04 00:14:43 +0300

    feat: update events mainnet addresses

--- src/events/constants.ts
+++ src/events/constants.ts
@@ -1,10 +1,14 @@
 import { Inputs } from '@mysten/sui/transactions';
 
-export const PACKAGE = '0x0';
+export const PACKAGE =
+  '0x72b79165ebc2a35e9166168b952c31aafc1531b73bc29c471dc4944b0054b88e';
+export const EVENTS_PACKAGE =
+  '0x72b79165ebc2a35e9166168b952c31aafc1531b73bc29c471dc4944b0054b88e';
 export const SHARED_OBJECTS = {
   FEE_CONFIG: Inputs.SharedObjectRef({
-    objectId: '0x0',
-    initialSharedVersion: '0',
+    objectId:
+      '0x0c11bc35a748db7971d9afbcf4fa26176e7277697c95e596b7cdc9b3e2df0c73',
+    initialSharedVersion: '436319355',
     mutable: true,
   }) as ReturnType<typeof Inputs.SharedObjectRef>,
 } as const;
