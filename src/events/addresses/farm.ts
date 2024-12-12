0a9e93cdf80d454eb55dd847bb88afaf9d326828
iamknownasfesal <mkircal957@gmail.com>
2024-12-12 21:53:58 +0300

    feat: update farm events

--- src/events/addresses/farm.ts
+++ src/events/addresses/farm.ts
@@ -1,6 +1,6 @@
-import { buildEvent } from '../utils';
+import { buildNewEvent } from '../utils';
 
-export const FARM_DEPOSIT_EVENT = buildEvent('farm', 'DepositEvent');
-export const FARM_WITHDRAW_EVENT = buildEvent('farm', 'WithdrawEvent');
-export const FARM_RELOCK_EVENT = buildEvent('farm', 'RelockEvent');
-export const FARM_HARVEST_EVENT = buildEvent('farm', 'HarvestEvent');
+export const FARM_DEPOSIT_EVENT = buildNewEvent('farm', 'DepositEvent');
+export const FARM_WITHDRAW_EVENT = buildNewEvent('farm', 'WithdrawEvent');
+export const FARM_RELOCK_EVENT = buildNewEvent('farm', 'RelockEvent');
+export const FARM_HARVEST_EVENT = buildNewEvent('farm', 'HarvestEvent');
